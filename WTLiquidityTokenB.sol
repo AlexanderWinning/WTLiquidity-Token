60d45f1a7c16e8c3759172aef2f1f59a099a0e94
WinningSwap <87269562+WinningSwap@users.noreply.github.com>
2021-08-27 21:57:47 +0100

    Create WTLiquidityTokenB.sol

---
+++ WTLiquidityTokenB.sol
@@ -0,0 +1,34 @@
+/// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12;
+
+import "./BEP20.sol";
+import "./Token.sol";
+
+contract WTLiquidity is BEP20("WTLiquidityTokenB", "WTLB"){
+
+    function PayingIn(uint In)public{
+        Token BNB = Token(0x08041eC6e81C0b2654C34209e25FeecB46A7D852);/// replace with token addresses
+        require(BNB.balanceOf(msg.sender) >= In, "Transfer Error :: Not enough BNB in your account");
+        if(balanceOf(address(this)) >= (In + 1000)){
+            BNB.approve(address(this), In);
+            BNB.transferFrom(msg.sender, address(this), In);
+            super.transferFrom(address(this), msg.sender, In);
+        }else if(balanceOf(address(this)) >= (In + 1)){
+            BNB.approve(address(this), In);
+            BNB.transferFrom(msg.sender, address(this), In);
+            super.transfer(msg.sender, In);
+            WT.transfer(0x08041eC6e81C0b2654C34209e25FeecB46A7D852, 1); /// To notify me to send the mint command 
+        }else{
+            WT.transfer(0x08041eC6e81C0b2654C34209e25FeecB46A7D852, 1); /// To notify me to send the mint command 
+        }
+    }
+    
+    function PayingOut(uint Out)public{
+        Token BNB = Token(0x08041eC6e81C0b2654C34209e25FeecB46A7D852);/// replace with token addresses
+        require(BNB.balanceOf(address(this)) >= Out, "Transfer Error :: Not enough BNB in the pool");
+        require(balanceOf(msg.sender) >= Out, "Transfer Error :: Not enough Liquidity tokens in your account");
+        super.approve(address(this), Out);
+        BNB.transferFrom(address(this), msg.sender, Out);
+        super.transferFrom(msg.sender, address(this), Out);
+    }
+}
