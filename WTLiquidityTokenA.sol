01c6c9bcb6d8c3a3df2dc19f950fb0e747d5e7a8
WinningSwap <87269562+WinningSwap@users.noreply.github.com>
2021-09-03 17:13:15 +0100

    Update WTLiquidityTokenA.sol

--- WTLiquidityTokenA.sol
+++ WTLiquidityTokenA.sol
@@ -7,8 +7,8 @@ import "./Token.sol";
 contract WTLiquidity is BEP20("WTLiquidityTokenA", "WTLA"){
 
     function PayingIn(uint In)public{
-        Token BNB = Token(0x08041eC6e81C0b2654C34209e25FeecB46A7D852); /// Replace with token addresses
-        Token WT = Token(0x08041eC6e81C0b2654C34209e25FeecB46A7D852); /// Replace with token addresses
+        Token BNB = Token(0x08041eC6e81C0b2654C34209e25FeecB46A7D852);/// Replace with token addresses
+        Token WT = Token(0x08041eC6e81C0b2654C34209e25FeecB46A7D852);/// Replace with token addresses
         require(BNB.balanceOf(msg.sender) >= In, "Transfer Error :: Not enough BNB in your account");
         require(WT.balanceOf(msg.sender) >= In, "Transfer Error :: Not enough WT in your account");
         if(balanceOf(address(this)) >= (In + 1000)){
@@ -40,11 +40,25 @@ contract WTLiquidity is BEP20("WTLiquidityTokenA", "WTLA"){
         WT.transferFrom(address(this), msg.sender, Out);
         super.transferFrom(msg.sender, address(this), Out);
     }
-    
-    function price()public returns(uint){ /// Used by exchanges to trade crypto
+
+    function PriceFind() public returns(uint){ /// WARNING MUST BE DIVIDED BY 100 AT THE CALLED END 
         Token BNB = Token(0x08041eC6e81C0b2654C34209e25FeecB46A7D852); /// Replace with token addresses
         Token WT = Token(0x08041eC6e81C0b2654C34209e25FeecB46A7D852); /// Replace with token addresses
-        uint Price = ((BNB.balanceOf(address(this)) / WT.balanceOf(address(this))) * 100); /// Multiplied by 100 to bypass the fixed point numbers implementation
+        uint Price = (WT.balanceOf(address(this)) / BNB.balanceOf(address(this)) * 100); /// Would mostly return a decimal which is not allowed as it is not implementented
         return Price;
+    }
+
+    function IPriceFind(int amount)public returns(uint){ /// WARNING :: MUST BE DIVIDED BY 100 AT THE CALLED END, Only issued by contracts that are actually trading
+        Token BNB = Token(0x08041eC6e81C0b2654C34209e25FeecB46A7D852); /// Replace with token addresses
+        Token WT = Token(0x08041eC6e81C0b2654C34209e25FeecB46A7D852); /// Replace with token addresses
+        uint K1 = (WT.balanceOf(address(this)) / BNB.balanceOf(address(this)) * 100); /// Would mostly return a decimal which is not allowed as it is not implementented
+        if(amount > 0){
+            uint Price = ((WT.balanceOf(address(this)) - ((K1 / 100) * (amount / 2)) / BNB.balanceOf(address(this)) + (amount / 2)) * 100); /// Likely to throw a decimal point 
+            /// Price = A1 - (K1 * (amount / 2)) / A2 + (amount / 2) My formula for buying using Automated Market Making
+        }else{
+            uint Price = ((WT.balanceOf(address(this)) + ((amount + (amount * -2)) / 2)) / BNB.balanceOf(address(this)) - ((1 / (k1 / 100)) * ((amount + (amount * -2)) / 2))) * 100); /// Likely to throw a decimal point
+            /// Price = A1 + (amount / 2) / A2 - (1 / K1) * (amount / 2) My formula for selling using Automated Market Making
+        }
+        return Price; 
     }
 }
